6ac249b0ec89048942a29b8ae5a5f30aba719fb0
jar812k6 <Jason@RamTechUSA.Com>
2019-01-05 16:52:49 -0500

    Update shot.php

--- demo/shot.php
+++ demo/shot.php
@@ -8,7 +8,41 @@ if (!isset($_GET['url']) || !isset($_GET['accessCode']) || $_GET['accessCode'] !
     exit;
 }
 
+//Set a random user agent to use in the request (Using the top 10 most common user agents from March 1st 2018
+$rand2 = rand(1,10);
+if($rand2 == '1'){
+$useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36';
+}ELSE if($rand2 == '2'){
+$useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36';
+}ELSE if($rand2 == '3'){
+$useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0';
+}ELSE if($rand2 == '4'){
+$useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36';
+}ELSE if($rand2 == '5'){
+$useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6';
+}ELSE if($rand2 == '6'){
+$useragent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36';
+}ELSE if($rand2 == '7'){
+$useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36';
+}ELSE if($rand2 == '8'){
+$useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36';
+}ELSE if($rand2 == '9'){
+$useragent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36';
+}ELSE if($rand2 == '10'){
+$useragent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0';
+}  
+
+//Load the screencapture system
 $screen = new Screen\Capture($_GET['url']);
+
+$screen->setOptions([
+    'ignore-ssl-errors' => 'true',
+    'disk-cache' => 'false',
+    
+]);
+
+//Set the useragent to be used
+$screen->setUserAgentString($useragent);
 
 if (isset($_GET['w'])) { // Width
     $screen->setWidth(intval($_GET['w']));
