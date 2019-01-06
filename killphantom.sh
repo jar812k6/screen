84d7b04a5fb7afc2b1dd4269706367eab5d5f9e0
jar812k6 <Jason@RamTechUSA.Com>
2019-01-06 15:43:52 -0500

    Update killphantom.sh

--- killphantom.sh
+++ killphantom.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
-#
-NAME=$1
-TIME=$2
-#
-for p in $(pidof $NAME)
+
+## Process name to be killed
+proc_name=phantomjs
+
+[ -n "$1" ] && proc_name=$1
+
+## Max execution time before process is considered stuck and eligible to be killed
+max_time=300
+
+[ -n "$2" ] && max_time=$2
+
+pids=$(pgrep $proc_name)
+
+for pid in $pids
 do
-UPTIME_PR=$(ps -p "$p" -o etime | tail -n 1|sed -e 's/^[ \t]//' | awk -F: '{print $160+$2}')
-PID=$(ps -p "$p" -o pid | tail -n 1 | sed -e 's/^[ \t]*//')
-if [ "$UPTIME_PR" -ge "$TIME" ]
+
+# elapsed=$(ps -p $pid -o etime | tail -1 | tr ':' ' ')
+
+elapsed=$(ps -p $pid -o etime= | tr '-' ':' | awk -F: '{ total=0; m=1; }
+{ for (i=0; i < NF; i++) {total += $(NF-i)*m; m *= i >= 2 ? 24 : 60 }}
+{print total}' )
+
+if (( $elapsed > $max_time ))
 then
-kill -9 $p
+kill $pid >/dev/null 2>&1 # Attempt softkill first
+sleep 0.1 # Give 0.1 seconds to quit gracefully.
+kill -9 $pid >/dev/null 2>&1
 fi
+
 done
+
 
